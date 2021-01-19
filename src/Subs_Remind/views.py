0f87da90b34d3202e8854f8644153f60027fc9cf
Sam Ebison <ebsa491@gmail.com>
2021-01-19 10:02:43 +0330

    routes are added to views.py

--- src/Subs_Remind/views.py
+++ src/Subs_Remind/views.py
@@ -3,8 +3,24 @@ The routes of the project. (mt`V` pattern)
 """
 
 from Subs_Remind import app
+from flask import render_template
 
 
 @app.route('/')
 def home():
-    return "Home"
+    return render_template("home/index.html")
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard/index.html")
+
+
+@app.route('/dashboard/new')
+def new():
+    return render_template("new/index.html")
+
+
+@app.route('/dashboard/edit')
+def edit():
+    return render_template("new/index.html")
